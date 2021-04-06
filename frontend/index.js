6b9b77a9f3714647259c39cbd4f5a75cd14f27b5
MattMrak <matthewmrakovcic@gmail.com>
2021-04-06 13:50:47 -0400

    Index.js Add Event Listener to Hole & Randomize where the holes will be displayed

--- frontend/index.js
+++ frontend/index.js
@@ -3,3 +3,9 @@ const hole = document.getElementById("hole");
 const character = document.getElementById("character");
 let jumping = 0;
 let counter = 0;
+
+hole.addEventListener('animationiteration', () => {
+    const random = -((Math.random() * 300) + 150);
+    hole.style.top = random + "px";
+    counter++;
+});
