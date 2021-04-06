33ccbeb961063d3829a0f71c53430523ddfcf200
MattMrak <matthewmrakovcic@gmail.com>
2021-04-06 13:48:10 -0400

    Index.js Setup Variables

---
+++ frontend/index.js
@@ -0,0 +1,5 @@
+const block = document.getElementById("block");
+const hole = document.getElementById("hole");
+const character = document.getElementById("character");
+let jumping = 0;
+let counter = 0;
