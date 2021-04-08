064baf7ad06a9728b7bd877acdb54a064e9c57e9
MattMrak <matthewmrakovcic@gmail.com>
2021-04-08 13:58:01 -0400

    ScoresController Create

--- backend/app/controllers/scores_controller.rb
+++ backend/app/controllers/scores_controller.rb
@@ -3,4 +3,8 @@ class ScoresController < ApplicationController
         #when game ends display a form with the users score & an empty field to insert their name w submit button
         @score = Score.new
     end
+    def create
+        #create & save the new score w the users name
+        @score = Score.save
+    end
 end
