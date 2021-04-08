20c783bf08976b8db8e2d63aa222d2124b9e9372
MattMrak <matthewmrakovcic@gmail.com>
2021-04-08 14:12:04 -0400

    Fix Order of Routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  get '/scores' => 'scores#new'
+  post '/scores' => 'scores#create'
+  
   resources :games
   resources :scores
 
-  get '/scores' => 'scores#new'
-  post '/scores' => 'scores#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
