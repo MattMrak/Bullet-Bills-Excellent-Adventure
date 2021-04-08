7b058c371a6bcea6d2c1a929bcf4890954d8dc6c
MattMrak <matthewmrakovcic@gmail.com>
2021-04-08 13:58:31 -0400

    ScoresController Index

--- backend/app/controllers/scores_controller.rb
+++ backend/app/controllers/scores_controller.rb
@@ -7,4 +7,11 @@ class ScoresController < ApplicationController
         #create & save the new score w the users name
         @score = Score.save
     end
+    def index
+        #after new score is created, display a list of all scores in descending order (highest -> lowest) and have the score that was just created highlited
+        @score = Score.all
+        render json: @score
+    end
 end
+
+#score DB has name & amount attributes
