a4d622947f35928d793ff8a5731155c8180abc20
MattMrak <matthewmrakovcic@gmail.com>
2021-04-13 13:52:09 -0400

    Ran Rake DB:Seed

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,2 +1,7 @@
 Game.destroy_all
-Game.reset_pk_sequences
+Game.reset_pk_sequence
+Score.destroy_all
+Score.reset_pk_sequence
+
+character = Game.create(image: "Code/Phase4/Bullet-Bills-Excellent-Adventure/backend/images/Bullet_Bill.jpg")
+block = Game.create(image: "Code/Phase4/Bullet-Bills-Excellent-Adventure/backend/images/Mario_Pipe.jpg")
