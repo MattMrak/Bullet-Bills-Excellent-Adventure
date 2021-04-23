5cba767a363daddfd1eb94b1763ae959aa4b5c73
MattMrak <matthewmrakovcic@gmail.com>
2021-04-23 12:04:41 -0400

    Set backgrounds & changed Amount in DB from t.string to t.integer

--- backend/db/migrate/20210405173548_create_scores.rb
+++ backend/db/migrate/20210405173548_create_scores.rb
@@ -2,7 +2,7 @@ class CreateScores < ActiveRecord::Migration[6.1]
   def change
     create_table :scores do |t|
       t.string :name
-      t.string :amount
+      t.integer :amount
       t.belongs_to :game, null: false, foreign_key: true
 
       t.timestamps
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 2021_04_05_173654) do
 
   create_table "scores", force: :cascade do |t|
     t.string "name"
-    t.string "amount"
+    t.integer "amount"
     t.integer "game_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
