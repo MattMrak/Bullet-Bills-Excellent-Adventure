1f9c04d1ab6b571bb7aa4f2e9a8e2d136e40742c
MattMrak <matthewmrakovcic@gmail.com>
2021-05-03 16:56:33 -0400

    Moved New Scores into Class Scores, added validations for Name on Score Form

---
+++ frontend/src/components/scores.js
@@ -0,0 +1,70 @@
+class Scores {
+  
+  constructor() {
+
+    document.getElementById("game").innerHTML = ""
+
+    let scoreBoard = document.createElement("div")
+    scoreBoard.className = "scores"
+    scoreBoard.innerHTML = `<h2>Score Board</h2>`
+
+    let score = document.getElementById("newScoreContainer")
+    score.style.display="block"
+
+    game.append(score)
+
+    let form = document.getElementById("newScoreForm");
+
+    form.addEventListener("submit", (event) => {
+        event.preventDefault()
+        let player = event.target.name.value
+        fetch("http://localhost:3000/scores", {
+            method: 'POST',
+            headers:  {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify({
+            name: player,
+            amount: `${counter}`,
+            game_id: 1
+        })
+        })
+        .then(response => response.json())
+        .then((newScore) => {
+            let newlyCreated = document.createElement("ol")
+            newlyCreated.innerHTML = `<br><h3>Your Score</h3> Name: ${newScore.name} | Score: ${newScore.amount}`
+            scoreBoard.append(newlyCreated)
+            event.target.reset()
+        })
+        fetchScores()
+    })
+
+    function fetchScores() {
+        game.append(scoreBoard)
+        fetch("http://localhost:3000/scores")
+        .then(response => response.json())
+        .then((scoreArray) => {
+            scoreArray.forEach((score) => {
+                takeOneScoreAndTurnItIntoOl(score)
+            })
+        })
+        function takeOneScoreAndTurnItIntoOl(scoreObj) {
+            let scoreOl = document.createElement("ol")
+            scoreOl.className = "item"
+            scoreOl.innerHTML = `Name: ${scoreObj.name} | Score: ${scoreObj.amount}`
+            scoreBoard.append(scoreOl)
+        }
+        let startGame = document.getElementById("startGame")
+        game.append(startGame)
+        startGame.onclick = function() {
+            restart()
+        }
+        function restart() {
+            document.location.href=""
+        }
+    };
+
+  }
+
+}
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -39,8 +39,8 @@ setInterval(function() {
     const holeTop = parseInt(window.getComputedStyle(hole).getPropertyValue("top"));
     const cTop = -(500 - characterTop);
     if ((characterTop > 480) || ((blockLeft < 20) && (blockLeft > -50) && ((cTop < holeTop) || (cTop > holeTop + 130)))) {
-        addNewScore()
         character.style.top = 100 + "px";
+        new Scores
     }
 },10);
 
@@ -60,63 +60,4 @@ function jump() {
         }
         jumpCount++;
     },10);
-}
-
-function addNewScore() {
-    let scoreBoard = document.createElement("div")
-    scoreBoard.className = "scores"
-    scoreBoard.innerHTML = `<h2>Score Board</h2>`
-    document.getElementById("game").innerHTML = ""
-    let score = document.getElementById("newScoreContainer")
-    score.style.display="block"
-    game.append(score)
-    form = document.getElementById("newScoreForm");
-    form.addEventListener("submit", (event) => {
-        event.preventDefault()
-        let player = event.target.name.value
-        fetch("http://localhost:3000/scores", {
-            method: 'POST',
-            headers:  {
-                "Content-Type": "application/json",
-                "Accept": "application/json"
-            },
-            body: JSON.stringify({
-            name: player,
-            amount: `${counter}`,
-            game_id: 1
-        })
-        })
-        .then(response => response.json())
-        .then((newScore) => {
-            let newlyCreated = document.createElement("ol")
-            newlyCreated.innerHTML = `<br><h3>Your Score</h3> Name: ${newScore.name} | Score: ${newScore.amount}`
-            scoreBoard.append(newlyCreated)
-            event.target.reset()
-        })
-        fetchScores()
-    })
-    function fetchScores() {
-        game.append(scoreBoard)
-        fetch("http://localhost:3000/scores")
-        .then(response => response.json())
-        .then((scoreArray) => {
-            scoreArray.forEach((score) => {
-                takeOneScoreAndTurnItIntoOl(score)
-            })
-        })
-        function takeOneScoreAndTurnItIntoOl(scoreObj) {
-            let scoreOl = document.createElement("ol")
-            scoreOl.className = "item"
-            scoreOl.innerHTML = `Name: ${scoreObj.name} | Score: ${scoreObj.amount}`
-            scoreBoard.append(scoreOl)
-        }
-        let startGame = document.getElementById("startGame")
-        game.append(startGame)
-        startGame.onclick = function() {
-            restart()
-        }
-        function restart() {
-            document.location.href=""
-        }
-    };
 }
